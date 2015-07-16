233918c6841707f980bef822e300804a8b024f59
Ioannis Kokkinis <info@upg.gr>
2015-07-16 20:38:05 +0100

    alts

---
+++ www/alts.php
@@ -0,0 +1,6 @@
+<a href="w.php?m3u8=<?php echo $_GET["m3u8"];?>&poster=<?php echo $_GET["poster"];?>">alternative 1</a>
+ - 
+<a href="v3/player.php?m3u8=<?php echo $_GET["m3u8"];?>&poster=<?php echo $_GET["poster"];?>">alternative 2</a>
+ - 
+ <a href="<?php echo $m3u8;?>" >alternative 3</a>
+ 
