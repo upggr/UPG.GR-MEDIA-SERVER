496bda67b730ad4c529068f61910294a0b6c99ec
Ioannis Kokkinis <info@upg.gr>
2015-07-16 20:57:18 +0100

    mods

--- nginx.sh
+++ nginx.sh
@@ -34,8 +34,6 @@ sed -i -- 's/replaceip/'"$ip"'/g' /usr/local/nginx/conf/nginx.conf
 sed -i -- 's/localhost/'"$ip"'/g' /usr/local/nginx/html/stream.xml
 rm -f /usr/local/nginx/conf/nginx.conf.default
 ln -s /usr/local/nginx/sbin/nginx nginx
-sudo wget -O /usr/local/nginx/conf/nginx.conf http://tools.upg.gr/rtmpserver/conf/nginx.conf
-sudo wget -O /etc/nginx/nginx.conf http://tools.upg.gr/rtmpserver/conf/nginx.conf
 sudo apt-get install ffmpeg -y
 sudo service nginx start
 sudo rm -rf ~/working
