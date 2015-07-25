27b5da68c6990409dd42146b50ce929645c70985
Ioannis Kokkinis <info@upg.gr>
2015-07-25 21:04:52 +0100

    c

--- nginx-8088.sh
+++ nginx-8088.sh
@@ -16,7 +16,7 @@ git clone git://github.com/upggr/nginx.git
 git clone git://github.com/upggr/nginx-rtmp-module.git
 git clone git://github.com/upggr/UPG.GR-MEDIA-SERVER.git
 cp ~/working/UPG.GR-MEDIA-SERVER/conf/nginx.txt /etc/init.d/nginx
-cp ~/working/UPG.GR-MEDIA-SERVER/conf/refresh.txt /etc/cron.daily/refreshwww
+cp ~/working/UPG.GR-MEDIA-SERVER/conf/refresh8088.txt /etc/cron.daily/refreshwww
 sudo chmod +x /etc/cron.daily/refreshwww
 sudo chmod +x /etc/init.d/nginx
 sudo /usr/sbin/update-rc.d -f nginx defaults
